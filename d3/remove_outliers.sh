def8f7451996856387d03750a78de723680a405b
= <=>
2016-02-17 16:44:43 -0600

    try again

--- d3/remove_outliers.sh
+++
@@ -1,1 +0,0 @@
-cat MSA_By_Quintiles.json | grep -v 'Maya' | grep -v 'Aguadilla' | grep -v 'Caguas' > MSA_By_Quintiles2.json
--- data/MSA_POPULATION_2014/create_msa.sh
+++
@@ -1,1 +0,0 @@
-cat CBSA_Population.csv | grep -v 'Micro Area' > MSA_Population.csv
